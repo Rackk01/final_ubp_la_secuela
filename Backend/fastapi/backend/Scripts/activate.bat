ecc69b062765da5d73b8bb260a2de3f663e40d5c
__Facud462__ <facud462@gmail.com>
2024-02-06 22:39:15 -0300

    cambios y errores manejados

--- Backend/fastapi/backend/Scripts/activate.bat
+++ Backend/fastapi/backend/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\xampp\htdocs\final_ubp\final_ubp_la_secuela-main\Backend\fastapi\backend
+set VIRTUAL_ENV=C:\Final_UBP\final_ubp_la_secuela-main\Backend\fastapi\backend
 
 if not defined PROMPT set PROMPT=$P$G
 
--- Backend/fastapi/main.py
+++ Backend/fastapi/main.py
@@ -1,1 +1,15 @@
-print("¡Hola, Mundo!")
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="UBP - Back Fastapi",
+    description="Alumno: Grupo de los 4",
+    version="1.0.0",
+)
+
+@app.get("/")
+def home():
+    return {"message": "Hola mundo"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, rubro: str = None):
+    return {"item_id": item_id, "rubro": rubro}
